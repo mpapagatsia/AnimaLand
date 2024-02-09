676f55bf6dce30812a02d8040501f8bfac5437ba
Maria Papagatsia <mariapap200396@gmail.com>
2024-02-09 19:07:41 +0200

    enhanced documentation

--- src/main/java/gr/mpapagatsia/animaland/animal/AnimalController.java
+++ src/main/java/gr/mpapagatsia/animaland/animal/AnimalController.java
@@ -4,6 +4,7 @@ import gr.mpapagatsia.animaland.animal.dto.AnimalDto;
 import gr.mpapagatsia.animaland.animal.dto.PagedDto;
 import gr.mpapagatsia.animaland.animal.dto.TrickDto;
 import gr.mpapagatsia.animaland.animal.model.Animal;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -26,12 +27,14 @@ public class AnimalController {
         this.animalService = animalService;
     }
 
+    @ApiOperation(value = "Get All", notes = "Returns a paginated list of all animals.")
     @GetMapping(name = "Get All Animals")
     public ResponseEntity<PagedDto> getAll(Pageable page) {
         log.info("Fetching the list of available animals.");
         return ResponseEntity.ok(PagedDto.fromPage(animalService.getAllAnimals(page)));
     }
 
+    @ApiOperation(value = "Create", notes = "Creates a new animal.")
     @PostMapping(name = "Create Animal", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Animal> create(@RequestBody AnimalDto animalDto) {
         log.info("Create animal: {} ", animalDto);
@@ -39,6 +42,7 @@ public class AnimalController {
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Do Trick", notes = "Request an animal to perform a trick.")
     @GetMapping(value = "{id}/doTrick", name = "Animal does a trick")
     public ResponseEntity<TrickDto> doTrick(@PathVariable String id) {
         log.info("Request animal with id {} to do a trick.", id);
@@ -46,6 +50,7 @@ public class AnimalController {
         return ResponseEntity.ok(animalService.doTrick(id));
     }
 
+    @ApiOperation(value = "Learn Trick", notes = "Request an animal to learn a new trick from the available tricks to its species, if any.")
     @PutMapping(value = "{id}/learnTrick", name = "Animal learns a new trick", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<TrickDto>> learnTrick(@PathVariable String id) {
         log.info("Animal with id {} is about to learn a new trick.", id);
